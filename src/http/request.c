1a4ec4e3b742b3822cbe4c72e2aa8c162cc2001b
Trung <46499481+imqt@users.noreply.github.com>
2020-11-18 13:21:57 -0800

    Update request.c

--- src/http/request.c
+++ src/http/request.c
@@ -1,1 +1,9 @@
 #include "request.h"
+
+int parse_request(char request[]) {
+    char GET_R[] = "GET";
+    for (int i = 0; i < 3; i++)
+        if (GET_R[i] != request[i])
+            return 0; // unsupported request
+    return 1; // GET request
+}
