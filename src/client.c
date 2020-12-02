67d133da0fdd3303b755ed475b332887bf00778e
Vivian Wu <wu.vivian00@gmail.com>
2020-12-02 03:48:23 -0800

    added request for demo on bsd

--- src/client.c
+++ src/client.c
@@ -28,15 +28,31 @@ int main(int argc, char *argv[])
     ssize_t request_len, reply_len;
     char buf[BUF_SIZE], tmp[BUF_SIZE];
     char end_code[5] = "end";
-    while((request_len = dc_read(STDIN_FILENO, buf, BUF_SIZE)) > 0)
-    {
-        dc_write(sfd, buf, request_len);
 
-        strncpy(tmp, buf, 3); if (strcmp(tmp, end_code) == 0) {break;}
-        
-        reply_len = dc_read(sfd, buf, BUF_SIZE);
-        dc_write(STDOUT_FILENO, buf, reply_len);
-    }
+    char* request = "GET / HTTP/1.1\n"
+                    "Host: 127.0.0.1:6969\n"
+                    "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0\n"
+                    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n"
+                    "Accept-Language: en-US,en;q=0.5\n"
+                    "Accept-Encoding: gzip, deflate\n"
+                    "Connection: keep-alive\n"
+                    "Upgrade-Insecure-Requests: 1";
+    size_t request_len = strlen(request);
+    dc_write(sfd, request, request_len);
+
+    //strncpy(tmp, buf, 3); if (strcmp(tmp, end_code) == 0) {break;}
+
+    reply_len = dc_read(sfd, buf, BUF_SIZE);
+    dc_write(STDOUT_FILENO, buf, reply_len);
+//    while((request_len = dc_read(STDIN_FILENO, buf, BUF_SIZE)) > 0)
+//    {
+//        dc_write(sfd, buf, request_len);
+//
+//        strncpy(tmp, buf, 3); if (strcmp(tmp, end_code) == 0) {break;}
+//
+//        reply_len = dc_read(sfd, buf, BUF_SIZE);
+//        dc_write(STDOUT_FILENO, buf, reply_len);
+//    }
 
     dc_write(STDOUT_FILENO, "Closing sfd!\n", 14);
 
