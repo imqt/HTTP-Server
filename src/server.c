a8aebf38ecaa525a9216c88ce3a7dcd6aa725421
imqt <quoctrung1220@gmail.com>
2020-12-02 03:38:51 -0500

    added so port can be reused after server crash ! no need to change every time ya screw up boiiii

--- src/server.c
+++ src/server.c
@@ -18,34 +18,15 @@
 #define SERVER_NAME "Team-7"
 #define DEFAULT_ROOT "../../rsc/"
 #define DEFAULT_404 "../../rsc/404.html"
-#define DEFAULT_PORT 6967 
+#define DEFAULT_PORT 6969 
 
 uint16_t port_number;
 
 void set_variables();
 
-void *dealer(void *vargp);
+void *t_dealer(void *vargp);
 
-void pdealer(int sfd) {
-    for(;;)
-    {
-        // Check if server is running with processes
-        // If yes, break.
-        int cfd = dc_accept(sfd, NULL, NULL);
-        char client_request[BUF_SIZE]; 
-        ssize_t request_len; 
-        while((request_len = dc_read(cfd, client_request, BUF_SIZE)) > 0)
-        {
-            char file_name[BUF_SIZE] = "../../rsc/";
-            int content_type_code = 0;
-            int request_code = parse_request(client_request, file_name, &content_type_code, request_len);
-            if (request_code) {
-                respond(cfd, file_name, content_type_code, request_code);
-            }
-        }
-        dc_close(cfd);
-    }
-}
+void p_dealer(int sfd);
 
 int main(int argc, const char * argv[])
 {
@@ -63,33 +44,34 @@ int main(int argc, const char * argv[])
     dc_bind(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
     dc_listen(sfd, BACKLOG);
 
+
+    int enable = 1;
+
+    if(setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+    {
+    perror("setsockopt(SO_REUSEADDR)");
+    }
     // pthread_t thread_id;
 
     // for (int i = 0; i < DEALERS; i++) {
-    //     pthread_create(&thread_id, NULL, dealer, (void *) &sfd);
+    //     pthread_create(&thread_id, NULL, t_dealer, (void *) &sfd);
     //     // dc_write(STDOUT_FILENO, "\n//////////////////////////////After responding\n", 50);
     // }
     // pthread_join(thread_id, NULL); // wait for the last thread to end
 
     pid_t child_pid, wpid;
     int ret, status;
-            printf("LET'S GO???\n");
-
     for (int i = 0; i < 5; i++) {
         child_pid = fork();
         if(child_pid == -1) { 
             perror("fork");
             exit(EXIT_FAILURE);
         } else if(child_pid == 0) { //Child
-            char *aaa = "HOHO";
-            dc_write(STDOUT_FILENO, aaa, strlen(aaa));
-            pdealer(sfd);
+            p_dealer(sfd);
             break;
         }   
     }
     do {
-        char *aaa = "HAHA\n";
-        dc_write(STDOUT_FILENO, aaa, strlen(aaa));
         wpid = waitpid(child_pid, &status, WUNTRACED);
         if (wpid == -1) {
             perror("waitpid");
@@ -97,13 +79,11 @@ int main(int argc, const char * argv[])
         }
         fprintf(stderr, "child exited, status=%d\n", WEXITSTATUS(status));
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-
     dc_close(sfd);
-
     return EXIT_SUCCESS;
 }
 
-void *dealer(void *vargp) {
+void *t_dealer(void *vargp) {
     int *sfd = (int *) vargp;
     for(;;)
     {
@@ -119,6 +99,27 @@ void *dealer(void *vargp) {
             int request_code = parse_request(client_request, file_name, &content_type_code, request_len);
             if (request_code) {
             	respond(cfd, file_name, content_type_code, request_code);
+            }
+        }
+        dc_close(cfd);
+    }
+}
+
+void p_dealer(int sfd) {
+    for(;;)
+    {
+        // Check if server is running with processes
+        // If yes, break.
+        int cfd = dc_accept(sfd, NULL, NULL);
+        char client_request[BUF_SIZE]; 
+        ssize_t request_len; 
+        while((request_len = dc_read(cfd, client_request, BUF_SIZE)) > 0)
+        {
+            char file_name[BUF_SIZE] = "../../rsc/";
+            int content_type_code = 0;
+            int request_code = parse_request(client_request, file_name, &content_type_code, request_len);
+            if (request_code) {
+                respond(cfd, file_name, content_type_code, request_code);
             }
         }
         dc_close(cfd);
--- src/shared.h
+++ src/shared.h
@@ -7,7 +7,7 @@
 
 // https://www.sciencedirect.com/topics/computer-science/registered-port#:~:text=Ports%200%20through%201023%20are,be%20used%20dynamically%20by%20applications.
 // /etc/services
-#define PORT 6969
+#define PORT 6967
 
 // Audio 	audio/mpeg
 //          audio/mpeg3
